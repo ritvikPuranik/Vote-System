7a3e7bccd70420d0712e223fb9b0a9c8f181e332
ritvik puranik <ritvikpuranik@gmail.com>
2023-11-05 12:27:02 +0530

    Complete project

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,8 +8,13 @@ contract Election {
         uint voteCount;
     }
 
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
     // Read/write candidates
     mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
 
     // Store Candidates Count
     uint public candidatesCount;
@@ -22,5 +27,22 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+        
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
