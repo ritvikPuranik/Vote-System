90fd96abbb49e59f567a8443ba1c071c02130097
ritvik puranik <ritvikpuranik@gmail.com>
2023-11-26 10:37:06 +0530

    Add Candidate option enable and code refactor. Need to fix the event part and redirect page for storing candidate details

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ App = {
   web3Provider: null,
   contracts: {},
   account: '0x0',
+  contractInstance: null,
 
   init: function() {
     return App.initWeb3();
@@ -32,86 +33,121 @@ App = {
     });
   },
 
-  render: function() {
-    var electionInstance;
+  isOwner: async() =>{
+    const owner = await App.contractInstance.owner();
+    console.log("owner od contract>>", owner);
+    if(App.account !== owner){
+      console.log("This account is not owner");
+      document.getElementById("add-candidate").style.display = "none";
+    }else{
+      document.getElementById("add-candidate").style.display = "block";
+    }
+  },
+
+  render: async() => {
+    // Load contract data
+    let instance = await App.contracts.Election.deployed();
+    App.contractInstance = instance;
+
     var loader = $("#loader");
     var content = $("#content");
-  
+    
     loader.show();
     content.hide();
-  
+    
     // Load account data
     web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
         App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
+        
       }
     });
+    await window.ethereum.enable();
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    App.account = accounts[0];
+    $("#accountAddress").html("Your Account: " + App.account);
+    await App.isOwner();
+    window.ethereum.on('accountsChanged', async function (accounts) {
+      App.account = accounts[0];
+      $("#accountAddress").html("Your Account: " + App.account);
+      await App.isOwner();
+    });
+
+    
   
-    // Load contract data
-    App.contracts.Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      return electionInstance.candidatesCount();
-    }).then(function(candidatesCount) {
+    try{
+      let candidatesCount = await instance.candidatesCount();
+  
       var candidatesResults = $("#candidatesResults");
       candidatesResults.empty();
-
+  
       var candidatesSelect = $('#candidatesSelect');
       candidatesSelect.empty();
   
       console.log("candidatesCount>>", candidatesCount.c[0]);
       for (var i = 1; i <= candidatesCount.c[0]; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
-          var id = candidate[0];
-          var name = candidate[1];
-          var voteCount = candidate[2];
+        let candidate = await instance.candidates(i);
+        var id = candidate[0];
+        var name = candidate[1];
+        var voteCount = candidate[2];
+        
+        // Render candidate Result
+        var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
+        candidatesResults.append(candidateTemplate)
   
-          // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
-          candidatesResults.append(candidateTemplate)
-  
-          // Render candidate ballot option
-          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-          candidatesSelect.append(candidateOption);
-        });
+        // Render candidate ballot option
+        var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+        candidatesSelect.append(candidateOption);
       }
-      return electionInstance.voters(App.account);
-    }).then(function(hasVoted) {
-      // Do not allow a user to vote
-      if(hasVoted) {
+      console.log("app account>", App.account);
+      
+      let hasVoted = await instance.voters(App.account);
+       // Do not allow a user to vote
+       if(hasVoted) {
         $('form').hide();
       }
       loader.hide();
       content.show();
-    }).catch(function(error) {
-      console.warn(error);
-    });
+
+    }catch(err){
+      console.log("error while rendering>>",err);
+    }
+
   },
 
-  castVote: function() {
+  addCandidate: async() =>{
+    let contractInstance = App.contractInstance;
+    console.log("contract instnce from addcandidate>", contractInstance);
+    let name = prompt("Enter Candidate Name");
+    try{
+      await contractInstance.addCandidate(name, {from: App.account});
+    }catch(err){
+      console.log("error while adding new candidate>>", err);
+    }
+    await App.render();
+
+  },
+
+  castVote: async () =>{
     var candidateId = $('#candidatesSelect').val();
-    App.contracts.Election.deployed().then(function(instance) {
-      return instance.vote(candidateId, { from: App.account });
-    }).then(function(result) {
-      // Wait for votes to update
-      $("#content").hide();
-      $("#loader").show();
-    }).catch(function(err) {
-      console.error(err);
-    });
+    await App.contractInstance.vote(candidateId, {from: App.account});
+    
+    // Wait for votes to update
+    $("#content").hide();
+    $("#loader").show();
   },
 
-  listenForEvents: function() {
-    App.contracts.Election.deployed().then(function(instance) {
-      instance.votedEvent({}, {
-        fromBlock: 'latest',
-        toBlock: 'latest'
-      }).watch(function(error, event) {
-        console.log("event triggered", event)
-        // Reload when a new vote is recorded
-        App.render();
-      });
+  listenForEvents: async()  =>{
+    let contractInstance = App.contractInstance;
+    contractInstance.votedEvent({}, {
+      fromBlock: 'latest',
+      toBlock: 'latest'
+    }).watch(function(error, event) {
+      console.log("event triggered", event)
+      // Reload when a new vote is recorded
+      App.render();
     });
+
   }
 };
 
